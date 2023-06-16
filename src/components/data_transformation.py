edb126f43d62f4ce96c3dce2a3d57c9721483d62
Gayathri Devi MS <59758501+msgayathridevi@users.noreply.github.com>
2023-06-16 15:08:52 +0530

    Data Transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,32 +1,39 @@
+# fetching data from data ingestion and transform features
+# main aim in data transformation is to feature engineering, data cleaning, convert feature types etc.
+# mostly same as done in eda (techniques used and transformations)
+
 import sys
 from dataclasses import dataclass
 
 import numpy as np 
 import pandas as pd
+
+# for pipeline like onehot encoder, standard scaler etc..
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
+# for missing values
+from sklearn.impute import SimpleImputer
+
 from src.exception import CustomException
 from src.logger import logging
+
 import os
 
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    # can also create model file
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
+    # This function si responsible for data trnasformation
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
-        
-        '''
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
@@ -39,17 +46,25 @@ class DataTransformation:
 
             num_pipeline= Pipeline(
                 steps=[
+                
+                # resp for handling missing values. median b'coz to handle outliers
                 ("imputer",SimpleImputer(strategy="median")),
+                
                 ("scaler",StandardScaler())
 
                 ]
             )
 
+            # in the same order of steps pipeline
             cat_pipeline=Pipeline(
 
                 steps=[
+                # most_frequent : mode
                 ("imputer",SimpleImputer(strategy="most_frequent")),
+
+                # OneHotEncoder as used in EDA due to less features and categories
                 ("one_hot_encoder",OneHotEncoder()),
+                
                 ("scaler",StandardScaler(with_mean=False))
                 ]
 
@@ -58,29 +73,29 @@ class DataTransformation:
             logging.info(f"Categorical columns: {categorical_columns}")
             logging.info(f"Numerical columns: {numerical_columns}")
 
+            # combine num and cat pipelines using ColumnTransformer
             preprocessor=ColumnTransformer(
                 [
                 ("num_pipeline",num_pipeline,numerical_columns),
                 ("cat_pipelines",cat_pipeline,categorical_columns)
-
                 ]
-
 
             )
 
+            # return the transformed data
             return preprocessor
         
         except Exception as e:
             raise CustomException(e,sys)
         
+    # parameters from data ingestion
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
-
             logging.info("Obtaining preprocessing object")
 
             preprocessing_obj=self.get_data_transformer_object()
@@ -88,18 +103,18 @@ class DataTransformation:
             target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df = test_df[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
@@ -108,11 +123,10 @@ class DataTransformation:
 
             logging.info(f"Saved preprocessing object.")
 
+            # save the transformer obj i.e. preprocessor.pkl
             save_object(
-
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessing_obj
-
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
             )
 
             return (
@@ -120,5 +134,6 @@ class DataTransformation:
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
+        
         except Exception as e:
             raise CustomException(e,sys)
