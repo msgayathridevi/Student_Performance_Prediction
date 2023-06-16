afd16134cb54fd0ba15692df1ef7e2755209a211
Gayathri Devi MS <59758501+msgayathridevi@users.noreply.github.com>
2023-06-16 16:17:44 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,13 @@
+'''
+    train teh data with diff regression models and see which works best for this data
+'''
+
 import os
 import sys
+
 from dataclasses import dataclass
 
+# since this is a regression problem try many regression algos and see the best for this data
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -9,34 +15,37 @@ from sklearn.ensemble import (
     RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object,evaluate_models
+from src.utils import save_object, evaluate_models
 
+# for all components config file
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
-
+        self.model_trainer_config = ModelTrainerConfig()
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
-                train_array[:,:-1],
+                train_array[:,:-1], 
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
+
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -46,6 +55,7 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -84,14 +94,18 @@ class ModelTrainer:
                 
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+            # evaluate_models func in the utils.py
+            model_report:dict = evaluate_models(X_train = X_train,
+                                                y_train = y_train,
+                                                X_test = X_test,
+                                                y_test = y_test,
+                                                models = models,
+                                                param = params)
             
-            ## To get best model score from dict
+            # to get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-            ## To get best model name from dict
-
+            # to get best model name from dict
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
@@ -102,18 +116,14 @@ class ModelTrainer:
             logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
+                file_path = self.model_trainer_config.trained_model_file_path,
+                obj=best_model # best_model will be craeted as pickle file
             )
 
-            predicted=best_model.predict(X_test)
+            predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
-
-
-
-            
+                        
         except Exception as e:
             raise CustomException(e,sys)
