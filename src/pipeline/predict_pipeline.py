8aec980c8ad72c4ee0e2dbd8278a7936baace577
Gayathri Devi MS <59758501+msgayathridevi@users.noreply.github.com>
2023-06-16 16:59:13 +0530

    Prediction Pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,30 +1,14 @@
+# web app to interact with the pkl files
+# the data gn in the web app interacts with the model/pkl files and yields the prediction
+
 import sys # for exeption handling
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 
-
-class PredictPipeline:
-    def __init__(self):
-        pass
-
-    def predict(self,features):
-        try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
-
-
-
+# maps the input data to the backend
 class CustomData:
-    def __init__(  self,
+    def __init__(self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -34,19 +18,14 @@ class CustomData:
         writing_score: int):
 
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
+    # return all the inputs as a data frame
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
@@ -64,3 +43,28 @@ class CustomData:
         except Exception as e:
             raise CustomException(e, sys)
 
+
+class PredictPipeline:
+    # empty constructor
+    def __init__(self):
+        pass
+
+    # model prediction using pkl files
+    def predict(self,features):
+        try:
+            model_path='artifacts\model.pkl'
+            
+            # preprocessor.pkl for handling categorical, feature scaling, preprocessing
+            preprocessor_path='artifacts\preprocessor.pkl'
+            
+            # load_object from the utils.py to load the pkl file
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            
+            data_scaled=preprocessor.transform(features)
+            prediction=model.predict(data_scaled)
+            
+            return prediction
+        
+        except Exception as e:
+            raise CustomException(e,sys)
