84cf813fe59800da6b3f5e613694ecf094ddc0c8
msgayathridevi <gayudevi.2002@gmail.com>
2023-06-16 10:52:57 +0530

    setup updates

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
+# this function will return the list of requirements
 def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
+       
     requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines() # avoid \n during the fetch
+        requirements = [req.replace("\n", "") for req in requirements]
 
         # to remove the -e .y in the requirements.txt while calling
         if HYPEN_E_DOT in requirements:
@@ -18,11 +17,12 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='generic_ml_project',
+name='stud_perf_pred',
 version='0.0.1',
 author='Gayathri Devi',
-author_email='gayudevi.2002@gmail.com',
-packages=find_packages(),
+author_email='msg3dv@gmail.com',
+# identifies source as a package itself, checks how many and whr __init_ files are present
+packages=find_packages(), # find_package to find and load settings from an external project
 # install_requires=['pandas','numpy','seaborn']
 install_requires=get_requirements('requirements.txt')
 )
